2a1e16539de0e7b93cf0ae5b2d611b9320d2d47c
James Zhang <jyz.james.zhang@gmail.com>
2022-07-08 20:52:28 -0700

    initial commit

---
+++ split/split/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  split
+//
+//  Created by James Zhang on 7/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
