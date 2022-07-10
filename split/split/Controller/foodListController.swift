16996559206c02172993c6568dbf250af2985b32
CollinQ <collinqian@ucsb.edu>
2022-07-10 00:45:27 -0700

    foodlistcontroller

---
+++ split/split/Controller/foodListController.swift
@@ -0,0 +1,29 @@
+//
+//  foodListController.swift
+//  split
+//
+//  Created by Collin Qian on 7/10/22.
+//
+
+import UIKit
+
+class foodListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
